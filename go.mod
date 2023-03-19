76e9a3abf543ede0bd09bd7f5c0c99398507ce6c
Manel Montilla <manel.montilla@gmail.com>
2023-03-19 13:06:29 +0100

    Version 1.0.0

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/manelmontilla/modb
 
 go 1.20
-
-require github.com/manelmontilla/modc v0.0.2
--- modb.go
+++ modb.go
@@ -1,17 +1,25 @@
 package modb
 
 import (
-	"fmt"
 	"runtime/debug"
-
-	"github.com/manelmontilla/modc"
 )
+
+const modName = "github.com/manelmontilla/modb"
 
 // CallMeMaybe It's hard to look right at you baby, but here's my number, so
 // call me, maybe? Hey, I just met you, and this is crazy.
 func CallMeMaybe() string {
-	modcV := modc.CallMeMaybe()
-	fmt.Printf("got modc version: %s\n", modcV)
+	return myVersion()
+}
+
+func myVersion() string {
 	v, _ := debug.ReadBuildInfo()
-	return v.Main.Version
+	version := "unknown"
+	for _, m := range v.Deps {
+		if m.Path == modName {
+			version = m.Version
+			break
+		}
+	}
+	return version
 }
